c01010b2fb608d30169ce0cb088ff1d62000aeb6
pratikNDL <pratikgoesai@gmail.com>
2024-10-13 13:24:21 +0530

    add institute route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,19 @@
 import { Hono } from 'hono'
+import { initPrisma } from './middlewares/initPrisma'
+import { PrismaClient } from '@prisma/client/extension'
+import { router } from './routes/router'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DTABASE_URL: string
+  },
+  Variables: {
+    prisma: PrismaClient
+  }
+}>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use('*', initPrisma)
+app.route('/api/v1', router)
+
 
 export default app
